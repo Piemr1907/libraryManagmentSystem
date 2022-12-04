554a1ecce839d58fadf9bc5bcf1451d89babf5cd
Piemr1907 <111633672+Piemr1907@users.noreply.github.com>
2022-12-04 23:12:35 +0530

    First commit

---
+++ LibraryManagmentSystem/src/main/java/com/demo/Application/VotingSystem1Application.java
@@ -0,0 +1,13 @@
+package com.demo.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSystem1Application.class, args);
+	}
+
+}
---
+++ LibraryManagmentSystem/src/test/java/com/demo/Application/VotingSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
